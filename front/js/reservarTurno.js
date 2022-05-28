15dfc3f92554c34df7458c11b1bf5c998114bd34
Rocio Poppe <rociocpoppe@gmail.com>
2022-05-28 08:46:25 -0300

    TF-49 se agregan funciones para guardar datos de medicos, obras sociales y especialidades seleccionadas

--- front/js/reservarTurno.js
+++ front/js/reservarTurno.js
@@ -34,15 +34,15 @@ let especialidades=[
         especialidad:"pediatra"
     },
     {
-        id:1,
+        id:2,
         especialidad:"medico clinico"
     },
     {
-        id:1,
+        id:3,
         especialidad:"nutricionista"
     },
     {
-        id:1,
+        id:4,
         especialidad:"oftalmologo"
     }
 ];
@@ -54,7 +54,7 @@ function mostrarMedicos(){
     // const respuesta= await fetch ("http://localhost:8080/medicos");
     // const medicos=await respuesta.json();
     for(let i=0; i<medicos.length;i++){
-       document.querySelector(".sugerenciasMedicos").innerHTML+='<div class="medicoIndividual">'+
+       document.querySelector(".sugerenciasMedicos").innerHTML+='<div class="medicoIndividual" onClick=guardarMedico('+medicos[i].id+')>'+
                     '<div class="ficha"> '+
                         '<img src="../img/dr'+[i+1]+'.jpeg" alt="">'+
                         '<div>'+
@@ -69,11 +69,19 @@ function mostrarMedicos(){
 
 mostrarMedicos();
 
+let med;
+function guardarMedico(id){
+    med=id;
+    console.log("medico "+ med);
+    return med;
+}
+
+
 function mostrarEspecialidades(){
     // const respuesta= await fetch ("http://localhost:8080/especialidades");
     // const especialidades=await respuesta.json();
     for(let i=0; i<especialidades.length;i++){
-        document.querySelector(".sugerenciasEspecialidad").innerHTML+= '<div class="fondoSugerencias">'
+        document.querySelector(".sugerenciasEspecialidad").innerHTML+= '<div class="fondoSugerencias" onClick=guardarEspecialidades('+especialidades[i].id+')>'
                                                                             + '<h3>'+ especialidades[i].especialidad+'</h3>'
                 +'</div>'
     }
@@ -81,14 +89,40 @@ function mostrarEspecialidades(){
 
 mostrarEspecialidades();
 
+let esp;
+function guardarEspecialidades(id){
+    esp=id;
+    console.log("esp "+ esp);
+    return esp;
+}
+
+
 async function mostrarObrasSociales(){
     const respuesta= await fetch ("http://localhost:8080/os");
     const obrasSociales=await respuesta.json();
     for(let i=0; i<obrasSociales.length;i++){
-        document.querySelector(".sugerenciasObraSocial").innerHTML+='<div class="fondoSugerencias">'+
+        document.querySelector(".sugerenciasObraSocial").innerHTML+='<div class="fondoSugerencias" onClick=guardarObrasSociales('+obrasSociales[i].id_os+')>'+
                     '<h3>'+obrasSociales[i].nombre+'</h3>'+
-                '</div>'
+                '</div>'       
     }
 }
 
 mostrarObrasSociales();
+
+let obrasSocial;
+function guardarObrasSociales(id){
+   obrasSocial=id;
+   console.log(obrasSocial);
+   return obrasSocial;
+}
+
+
+document.querySelector("#botonAplicar").addEventListener("click", redirigir);
+
+
+function redirigir(){ 
+    let idmedico=guardarMedico(med);
+    let idespecialidad=guardarEspecialidades(esp);
+    window.location.href = "../html/visualizarHorariosMedico.html?idMed="+idmedico+"&idEspecialidad="+idespecialidad+ "&idOS=" + guardarObrasSociales(obrasSocial);
+}
+
