e72ca52aab1cc2fb98cb964049ecbd2024df3e82
Maximiliano Santomil <54724624+maxisantomil@users.noreply.github.com>
2022-05-29 12:34:22 -0300

    se agrega tabla horarios de tarde

--- front/js/seleccionarHora.js
+++ front/js/seleccionarHora.js
@@ -1,3 +1,6 @@
 function seleccionar_turno(){
     console.log("seleccionado");
+    document.querySelector().classList.toggle("fichaSelected");
 }
+
+document.querySelector(".day").addEventListener("click", seleccionar_turno);
