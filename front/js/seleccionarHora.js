83c8f5e73edba310c3ef7353d53a6dc592224378
Agustin Groh <agusgroh@gmail.com>
2022-05-29 15:28:55 -0300

    TF-71 Agrega linkeo de botones para volver atras

--- front/js/seleccionarHora.js
+++ front/js/seleccionarHora.js
@@ -3,3 +3,11 @@ function seleccionar_turno(){
 }
 
 document.querySelector(".day").addEventListener("click", seleccionar_turno());
+
+document.querySelector("#botonVolver").addEventListener("click",()=>{
+    const myurl= window.location.href;
+    let url = new URL(myurl);
+    let params = new URLSearchParams(url.search);
+    let idMed = params.get('idMed')
+    window.location.href=`../html/visualizarHorariosMedico.html?idMed=${idMed}`
+});
