76b4aca17cf4363a336c0910320f4c05813708d6
rociocpoppe <61523475+rociocpoppe@users.noreply.github.com>
2022-07-02 10:05:48 -0300

    TF-75 agregar pantalla turnos proxima semana
    * TF-75 se agrega pantalla visualización turnos de la próxima semana
    
    * TF-75 se agrega función para guardar días seleccionados y agregar 7 días proximos
    
    * TF-75 se arregla boton turnos no disponibles y se guarda en el js turnos no disponibles fechas y turnos del  dia
    
    * TF-75 se realiza redirigir en caso de que el medico seleccionado no tenga turnos disponibles
    
    TF-75 se agrega función para chequear si el médico esta disponible

--- front/js/seleccionarHora.js
+++ front/js/seleccionarHora.js
@@ -1,3 +1,26 @@
+const myurl= window.location.href;
+let url = new URL(myurl);
+let params = new URLSearchParams(url.search);
+let idMedico = params.get('idMed')
+let fechaDesde=params.get('fechaDesde');
+let fechaHasta=params.get('fechaHasta');
+
+document.addEventListener('DOMContentLoaded', function() {
+    agregarDias(fechaDesde,fechaHasta, 8);
+});
+
+function agregarDias(fechaDesde,fechaHasta, dias){
+    var desde = new Date(fechaDesde);
+    var hasta=new Date(fechaHasta);
+    desde.setDate(desde.getDate() + dias);
+    hasta.setDate(hasta.getDate() + dias);
+    let textoDesde=desde.toLocaleDateString();
+    let textoHasta=hasta.toLocaleDateString();
+    document.getElementById("proximaSemana").innerHTML="<p>Horarios Disponibles semana del "+ textoDesde+ " al " + textoHasta+"</p>";
+    
+}
+
+
 function seleccionar_turno(){
     console.log("seleccionado");
 }
